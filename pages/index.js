efd85c114fa2b8677931d43caae5f32f2f9a5eaa
Agailh <agailham86@gmail.com>
2022-08-08 15:21:19 +0700

    del import

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Main from "./components/Main";
 
 import Navbar from "./components/Navbar";
-import Project from "./components/Project";
+
 import Skills from "./components/Skills";
 
 export default function Home() {
